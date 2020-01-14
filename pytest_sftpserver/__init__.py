87025bcf8b1701752f652aefa6d70cff3884ed59
Krzysztof Jagiello <me@kjagiello.com>
2020-01-14 18:57:12 +0100

    Fix linting

--- pytest_sftpserver/__init__.py
+++ pytest_sftpserver/__init__.py
@@ -1,2 +1,3 @@
 from pytest_sftpserver.decorator import sftpserver_factory  # noqa: F401
+
 from .__version__ import VERSION, get_version  # noqa: F401
--- pytest_sftpserver/__version__.py
+++ pytest_sftpserver/__version__.py
@@ -2,4 +2,4 @@ VERSION = (1, 3, 0)
 
 
 def get_version():
-    return '.'.join(str(i) for i in VERSION)
+    return ".".join(str(i) for i in VERSION)
