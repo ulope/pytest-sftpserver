25d8170056666d1e08e58cbe44517a0847380aa9
Krzysztof Jagiello <me@kjagiello.com>
2020-01-14 18:46:32 +0100

    Move the version constant
    Previously, it being in `__init__.py` caused setup to fail because of it
    triggering import of `paramiko`.

--- pytest_sftpserver/__init__.py
+++ pytest_sftpserver/__init__.py
@@ -1,7 +1,2 @@
 from pytest_sftpserver.decorator import sftpserver_factory  # noqa: F401
-
-VERSION = (1, 3, 0)
-
-
-def get_version():
-    return ".".join(str(i) for i in VERSION)
+from .__version__ import VERSION, get_version  # noqa: F401
